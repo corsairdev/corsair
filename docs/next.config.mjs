9634f6d1f343374dc3a6eddb9a671f2055e13c31
Dev Jain <devrjain32200@gmail.com>
2025-12-02 09:45:31 -0500

    update vercel.json in docs folder

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -3,7 +3,6 @@ import { createMDX } from 'fumadocs-mdx/next'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracingRoot: __dirname,
 }
 
 const withMDX = createMDX()
