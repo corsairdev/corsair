49397c4a3826d920dbf354dbb3b29cf38d81bd80
Dev Jain <devrjain32200@gmail.com>
2025-12-02 09:42:25 -0500

    fix for next.config.mjs to deploy docs

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -3,6 +3,7 @@ import { createMDX } from 'fumadocs-mdx/next'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingRoot: __dirname,
 }
 
 const withMDX = createMDX()
