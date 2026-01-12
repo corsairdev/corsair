1b7a0c6f2ab9ae6df737ce159c4be8983af8341d
Mukul yadav <mukulydv15@gmail.com>
2026-01-12 19:58:02 +0530

    feat: introduce new database schema for corsair connections, events, and providers; update dotenv dependency and adjust multi-tenancy configuration

--- demo/corsair-plugins/src/test.ts
+++ demo/corsair-plugins/src/test.ts
@@ -1,11 +1,11 @@
 import { corsair } from './';
 
 const main = async () => {
-	const res = await corsair
-		.withTenant('j')
-		.slack.channels.deleteByResourceId('123');
-
+	const res = await corsair.slack.postMessage({
+		channel: 'C0A3ZTB9X7X',
+		text: 'Hello, world!',
+	});
 	console.log(res);
 };
 
-// main();
+main();
