ba4e99c832fc6b3ac71b562693be9943a81e6694
Dev Jain <devrjain32200@gmail.com>
2026-01-21 15:17:48 -0500

    update docs a bit and test error handling again

--- demo/corsair-plugins/src/test.ts
+++ demo/corsair-plugins/src/test.ts
@@ -3,7 +3,9 @@ import { corsair } from './';
 const main = async () => {
 	const teset = await corsair
 		.withTenant('default-1')
-		.slack.api.channels.archive({ channel: '123' });
+		.slack.api.channels.archive({
+			channel: '123',
+		});
 };
 
 // biome-ignore lint/nursery/noFloatingPromises: this is just for cli testing right now
