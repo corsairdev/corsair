49402d2235fba96ff4bf547eb38a60f267b14102
Dev Jain <devrjain32200@gmail.com>
2026-02-11 18:43:21 -0500

    update a few readme's

--- demo/corsair-fly/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	experimental: {
-		serverComponentsExternalPackages: ['pg', 'drizzle-orm'],
-	},
-	output: 'standalone',
-};
-
-export default nextConfig;
--- demo/corsair-fly/src/app/api/inngest/route.ts
+++
@@ -1,8 +0,0 @@
-import { serve } from 'inngest/next';
-import { inngest } from '@/server/inngest/client';
-import { functions } from '@/server/inngest/functions';
-
-export const { GET, POST, PUT } = serve({
-	client: inngest,
-	functions,
-});
