1c48105e8c94293cf49c38317bb973e2a92ffd22
Dev Jain <devrjain32200@gmail.com>
2026-02-17 13:41:29 -0500

    type fix for trpc router in demo

--- demo/corsair-plugins/src/server/api/root.ts
+++ demo/corsair-plugins/src/server/api/root.ts
@@ -2,7 +2,7 @@ import { channelsRouter } from './routers/channels';
 import { healthRouter } from './routers/health';
 import { createTRPCRouter } from './trpc';
 
-export const appRouter = createTRPCRouter({
+export const appRouter: ReturnType<typeof createTRPCRouter> = createTRPCRouter({
 	health: healthRouter,
 	channels: channelsRouter,
 });
