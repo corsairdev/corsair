d5be32cae4d333e527d1249dd055644e4c1dfaac
Dev Jain <devrjain32200@gmail.com>
2026-02-19 16:57:03 -0500

    fix api call for linear and increment package for deployment

--- demo/corsair-plugins/src/scripts/test-channels.ts
+++ demo/corsair-plugins/src/scripts/test-channels.ts
@@ -2,11 +2,35 @@ import { corsair } from '@/server/corsair';
 import 'dotenv/config';
 
 const main = async () => {
+	// Create a Linear issue in a team. This example demonstrates creating a new issue with a title, description, and priority level.
 	// const res = await corsair.keys.gmail.get_topic_id();
 
-	const res = await corsair.withTenant('').gmail.keys.get_something_else();
+	const allLinearTeams = await corsair.linear.api.teams.list({});
 
-	console.log(res);
+	const teams = allLinearTeams.nodes.map((team) => ({
+		teamId: team.id,
+		teamName: team.name,
+	}));
+
+	if (teams.length > 1) {
+		const teamNames = teams.map((team) => team.teamName);
+		console.log('Which team? Here is a list of the teams', teamNames);
+
+		return;
+	} else if (teams.length === 0) {
+		console.log('No Linear teams found');
+
+		return;
+	}
+
+	const postedIssue = await corsair.linear.api.issues.create({
+		teamId: teams[0]?.teamId!,
+		title: 'This is the title of the Linear issue',
+	});
+
+	console.log(postedIssue.id);
+
+	return;
 };
 
 main();
