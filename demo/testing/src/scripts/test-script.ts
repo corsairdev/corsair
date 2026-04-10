e62af6ded7330e5c8f9e0eaf6190af6c98b713d7
Mukul yadav <mukulydv15@gmail.com>
2026-04-10 19:59:21 +0530

    ref: remove console

--- demo/testing/src/scripts/test-script.ts
+++ demo/testing/src/scripts/test-script.ts
@@ -5,7 +5,6 @@ const main = async () => {
     // write any test scripts here
     // const res = await corsair -> fill in the rest
     const res = await corsair.gmail.api.messages.list({})
-    console.log(res, 'res')
 };
 
 main();
