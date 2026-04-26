847af7038746977009083601900a7b9154ce1aa4
PulkitChaudhari <pulkitchaudhari@gmail.com>
2026-04-26 19:16:27 +0530

    fix: adding constraint on amount and removing comments

--- packages/razorpay/schema/database.ts
+++ packages/razorpay/schema/database.ts
@@ -61,18 +61,18 @@ export const RazorpayPayoutSchema = z
 		id: z.string(),
 		entity: z.literal('payout'),
 		fund_account_id: z.string(),
-		amount: z.number(), // in paise, min value is 100
+		amount: z.number().int().positive().min(100),
 		currency: z.string(),
-		notes: RazorpayNotesSchema.optional(), // max 15 kv pairs, 256 chars max
+		notes: RazorpayNotesSchema.optional(),
 		fees: z.number().optional(),
 		tax: z.number().optional(),
 		status: z.string(),
 		utr: z.string().nullable().optional(),
 		mode: z.string().optional(),
 		purpose: z.string().optional(),
-        reference_id: z.string().nullable().optional(), // max 40 chars
+        reference_id: z.string().nullable().optional(),
         debit_account_number: z.string().optional(),
-		narration: z.string().nullable().optional(), // max 30 chars a-z, A-Z, 0-9 & space.
+		narration: z.string().nullable().optional(),
 		batch_id: z.string().nullable().optional(),
 		status_details: RazorpayPayoutStatusDetailsSchema.nullable().optional(),
         created_at: z.number().optional(),
