3849d437e3e93b6b87eac7f5ccab8e3c4d05a492
Nisarg Patel <nisargpatel460@gmail.com>
2026-06-05 22:05:19 +0530

    Instagram Integration Test

--- packages/instagram/integration.test.ts
+++ packages/instagram/integration.test.ts
@@ -24,7 +24,7 @@ import {
 import { instagram } from './index';
 
 describe(
-    'Instagram Auth Integration',
+    'Instagram Integration Test',
     () => {
 
         async function createInstagramClient() {
