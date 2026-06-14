a6968f5be9a8907eb9cbd88c375bd3a192152b8d
ambikeesshh <ambikesh2206@gmail.com>
2026-06-14 22:54:53 +0530

    drop supabase stub

--- packages/supabase/schema/database.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-// TODO: Define your database entities here
-// export const SupabaseExample = z.object({
-// 	id: z.string(),
-// 	name: z.string(),
-// 	created_at: z.coerce.date().nullable().optional(),
-// });
-// export type SupabaseExample = z.infer<typeof SupabaseExample>;
