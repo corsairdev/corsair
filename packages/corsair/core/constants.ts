0fc22462818a2c729468e37d64a310173cf4d714
Nisarg Patel <nisargpatel460@gmail.com>
2026-06-25 14:36:03 +0530

    added instagram plugin in providerDisplayNames

--- packages/corsair/core/constants.ts
+++ packages/corsair/core/constants.ts
@@ -145,6 +145,7 @@ export const ProviderDisplayNames = {
 	zendesk: 'Zendesk',
 	zohomail: 'Zoho Mail',
 	zoom: 'Zoom',
+	instagram: 'Instagram'
 } as const satisfies Record<(typeof BaseProviders)[number], string>;
 
 export function formatProviderDisplayName(plugin: string): string {
