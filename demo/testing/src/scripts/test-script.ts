c77dcf2e6cc3d2cadb4d3a9105179e1a1f8a59b0
Aman Raj <amanraj12.ar@gmail.com>
2026-07-03 22:27:32 +0530

    test: wire Apify into demo testing

--- demo/testing/src/scripts/test-script.ts
+++ demo/testing/src/scripts/test-script.ts
@@ -2,26 +2,19 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: '../.env' });
 
-import { corsair } from '@/server/corsair';
-
-async function setInstagramCredentials() {
-	const { FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, IG_ACCESS_TOKEN } = process.env;
-
-	if (FACEBOOK_APP_ID) {
-		await corsair.keys.instagram.set_client_id(FACEBOOK_APP_ID);
-	}
-	if (FACEBOOK_APP_SECRET) {
-		await corsair.keys.instagram.set_client_secret(FACEBOOK_APP_SECRET);
-	}
-	if (IG_ACCESS_TOKEN) {
-		await corsair.instagram.keys.set_access_token(IG_ACCESS_TOKEN);
-	}
-}
-
 const main = async () => {
-	const res = await corsair.slack.api.messages.post({
-		channel: 'general',
-		text: 'hello',
+	if (!process.env.APIFY_API_KEY) {
+		console.log('Skipping Apify demo test: APIFY_API_KEY is not set.');
+		return;
+	}
+
+	const { corsair } = await import('@/server/corsair');
+	const result = await corsair.apify.api.users.meGet({});
+	const data = result.data as { id?: string; username?: string };
+
+	console.log('Apify user lookup succeeded', {
+		id: data.id,
+		username: data.username,
 	});
 };
 
