71cbfb1d4467d3b24ff3e45138cb682c147b7020
ambikeesshh <ambikesh2206@gmail.com>
2026-07-15 22:03:03 +0530

    revert(apify): drop demo/testing wiring out of plugin scope

--- demo/testing/src/scripts/test-script.ts
+++ demo/testing/src/scripts/test-script.ts
@@ -2,19 +2,26 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: '../.env' });
 
-const main = async () => {
-	if (!process.env.APIFY_API_KEY) {
-		console.log('Skipping Apify demo test: APIFY_API_KEY is not set.');
-		return;
+import { corsair } from '@/server/corsair';
+
+async function setInstagramCredentials() {
+	const { FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, IG_ACCESS_TOKEN } = process.env;
+
+	if (FACEBOOK_APP_ID) {
+		await corsair.keys.instagram.set_client_id(FACEBOOK_APP_ID);
 	}
+	if (FACEBOOK_APP_SECRET) {
+		await corsair.keys.instagram.set_client_secret(FACEBOOK_APP_SECRET);
+	}
+	if (IG_ACCESS_TOKEN) {
+		await corsair.instagram.keys.set_access_token(IG_ACCESS_TOKEN);
+	}
+}
 
-	const { corsair } = await import('@/server/corsair');
-	const result = await corsair.apify.api.users.meGet({});
-	const data = result.data as { id?: string; username?: string };
-
-	console.log('Apify user lookup succeeded', {
-		id: data.id,
-		username: data.username,
+const main = async () => {
+	const res = await corsair.slack.api.messages.post({
+		channel: 'general',
+		text: 'hello',
 	});
 };
 
--- demo/testing/src/server/corsair.ts
+++ demo/testing/src/server/corsair.ts
@@ -3,12 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
 
 import { agentql } from '@corsair-dev/agentql';
-import { apify } from '@corsair-dev/apify';
 import { gmail } from '@corsair-dev/gmail';
 import { googlecalendar } from '@corsair-dev/googlecalendar';
 import { googlesheets } from '@corsair-dev/googlesheets';
 import { hubspot } from '@corsair-dev/hubspot';
-import { instagram } from '@corsair-dev/instagram';
 import { linear } from '@corsair-dev/linear';
 import { onedrive } from '@corsair-dev/onedrive';
 import { sharepoint } from '@corsair-dev/sharepoint';
@@ -59,9 +57,6 @@ export const corsair = createCorsair({
 		hubspot(),
 		agentql({
 			key: process.env.AGENTQL_API_KEY,
-		}),
-		apify({
-			key: process.env.APIFY_API_KEY,
 		}),
 		twilio(),
 		vapi({
