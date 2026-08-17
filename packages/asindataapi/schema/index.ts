25d01760df7364f2bed1ace59113bc9701d9e485
Dhirender Choudhary <Dhirenderchoudhary0001@gmail.com>
2026-08-18 05:18:08 +0530

    feat(apaleo): implement Inventory V1 ops from official swagger

--- packages/asindataapi/schema/index.ts
+++
@@ -1,9 +0,0 @@
-import { AsinDataApiCollection, AsinDataApiResultSet } from './database';
-
-export const AsinDataApiSchema = {
-	version: '1.0.0',
-	entities: {
-		collections: AsinDataApiCollection,
-		resultSets: AsinDataApiResultSet,
-	},
-} as const;
--- packages/asindataapi/webhooks/index.ts
+++
@@ -1,7 +0,0 @@
-import { collectionCompleted } from './collection-completed';
-
-export const CollectionWebhooks = {
-	collectionCompleted,
-};
-
-export * from './types';
