1c5a6bb790c8eba71251bcb501a1d4925cae27ca
ambikeesshh <ambikesh2206@gmail.com>
2026-08-19 22:26:16 +0530

    test(attio): pin generated endpoint route contracts

--- packages/attio/endpoints/generated.test.ts
+++ packages/attio/endpoints/generated.test.ts
@@ -385,25 +385,505 @@ describe('Attio REST API verification', () => {
 			thread_id: 'thr-1',
 		};
 
-		for (const [name, fn] of Object.entries(Generated)) {
-			if (typeof fn === 'function') {
-				it(`should invoke ${name} with a concrete Attio route`, async () => {
-					const res = (await fn(
-						localMockCtx as never,
-						pathFixture as never,
-					)) as MockCall;
-					expect(res).toBeDefined();
-					expect(res.url).toMatch(/^\/v2\//);
-					expect(res.url).not.toContain('{');
-					expect(res.url).not.toMatch(/\/\//);
-					expect(res.url).not.toMatch(
-						/unknown|objectss|attributess|companiess|\/lists\/list(?:\/|$)/,
-					);
-					expect(['GET', 'POST', 'PUT', 'PATCH', 'DELETE']).toContain(
-						res.options.method,
-					);
-				});
-			}
+		const expectedRoutes: Record<
+			string,
+			{ url: string; method: string; omitFromBody?: string[] }
+		> = {
+			putV2ListsListEntries: {
+				url: '/v2/lists/list-1/entries',
+				method: 'PUT',
+				omitFromBody: ['list', 'list_id'],
+			},
+			assertPerson: {
+				url: '/v2/objects/people/records',
+				method: 'PUT',
+			},
+			putV2ObjectsObjectRecords: {
+				url: '/v2/objects/companies/records',
+				method: 'PUT',
+			},
+			assertUserRecord: {
+				url: '/v2/objects/users/records',
+				method: 'PUT',
+			},
+			assertWorkspace: {
+				url: '/v2/objects/workspaces/records',
+				method: 'PUT',
+			},
+			createAttribute: {
+				url: '/v2/lists/list-1/attributes',
+				method: 'POST',
+				omitFromBody: ['object', 'list', 'list_id'],
+			},
+			createComment: {
+				url: '/v2/comments',
+				method: 'POST',
+			},
+			createCompany: {
+				url: '/v2/objects/companies/records',
+				method: 'POST',
+			},
+			createDealRecord: {
+				url: '/v2/objects/deals/records',
+				method: 'POST',
+			},
+			createEntry: {
+				url: '/v2/lists/list-1/entries',
+				method: 'POST',
+				omitFromBody: ['list', 'list_id'],
+			},
+			createList: {
+				url: '/v2/lists',
+				method: 'POST',
+			},
+			postV2ListsListEntries: {
+				url: '/v2/lists/list-1/entries',
+				method: 'POST',
+				omitFromBody: ['list', 'list_id'],
+			},
+			createNote: {
+				url: '/v2/notes',
+				method: 'POST',
+			},
+			createObject: {
+				url: '/v2/objects',
+				method: 'POST',
+			},
+			createPerson: {
+				url: '/v2/objects/people/records',
+				method: 'POST',
+			},
+			createRecord: {
+				url: '/v2/objects/companies/records',
+				method: 'POST',
+			},
+			createSelectOption: {
+				url: '/v2/lists/list-1/attributes/email_addresses/options',
+				method: 'POST',
+				omitFromBody: [
+					'object',
+					'list',
+					'list_id',
+					'attribute',
+					'attribute_id',
+				],
+			},
+			createStatus: {
+				url: '/v2/objects/companies/attributes/email_addresses/statuses',
+				method: 'POST',
+			},
+			createTask: {
+				url: '/v2/tasks',
+				method: 'POST',
+			},
+			createUserRecord: {
+				url: '/v2/objects/users/records',
+				method: 'POST',
+			},
+			createWebhook: {
+				url: '/v2/webhooks',
+				method: 'POST',
+			},
+			createWorkspaceRecord: {
+				url: '/v2/objects/workspaces/records',
+				method: 'POST',
+			},
+			postV2ObjectsObjectRecords: {
+				url: '/v2/objects/companies/records',
+				method: 'POST',
+			},
+			deleteComment: {
+				url: '/v2/comments/cmt-1',
+				method: 'DELETE',
+			},
+			deleteCompany: {
+				url: '/v2/objects/companies/records/rec-1',
+				method: 'DELETE',
+			},
+			deleteDeal: {
+				url: '/v2/objects/deals/records/rec-1',
+				method: 'DELETE',
+			},
+			deleteEntry: {
+				url: '/v2/lists/list-1/entries/ent-1',
+				method: 'DELETE',
+			},
+			deleteNote: {
+				url: '/v2/notes/note-1',
+				method: 'DELETE',
+			},
+			deletePerson: {
+				url: '/v2/objects/people/records/rec-1',
+				method: 'DELETE',
+			},
+			deleteRecord: {
+				url: '/v2/objects/companies/records/rec-1',
+				method: 'DELETE',
+			},
+			deleteRecordById: {
+				url: '/v2/objects/companies/records/rec-1',
+				method: 'DELETE',
+			},
+			deleteTask: {
+				url: '/v2/tasks/task-1',
+				method: 'DELETE',
+			},
+			deleteUser: {
+				url: '/v2/objects/users/records/rec-1',
+				method: 'DELETE',
+			},
+			deleteWebhook: {
+				url: '/v2/webhooks/wh-1',
+				method: 'DELETE',
+			},
+			deleteWorkspaceRecord: {
+				url: '/v2/objects/workspaces/records/rec-1',
+				method: 'DELETE',
+			},
+			findRecord: {
+				url: '/v2/objects/companies/records/rec-1',
+				method: 'GET',
+			},
+			getAttribute: {
+				url: '/v2/lists/list-1/attributes/email_addresses',
+				method: 'GET',
+			},
+			getComment: {
+				url: '/v2/comments/cmt-1',
+				method: 'GET',
+			},
+			getCompany: {
+				url: '/v2/objects/companies/records/rec-1',
+				method: 'GET',
+			},
+			getSelf: {
+				url: '/v2/self',
+				method: 'GET',
+			},
+			getDealRecord: {
+				url: '/v2/objects/deals/records/rec-1',
+				method: 'GET',
+			},
+			getList: {
+				url: '/v2/lists/list-1',
+				method: 'GET',
+			},
+			getListEntry: {
+				url: '/v2/lists/list-1/entries/ent-1',
+				method: 'GET',
+			},
+			getNote: {
+				url: '/v2/notes/note-1',
+				method: 'GET',
+			},
+			getObject: {
+				url: '/v2/objects/obj-1',
+				method: 'GET',
+			},
+			peopleGetPerson: {
+				url: '/v2/objects/people/records/rec-1',
+				method: 'GET',
+			},
+			getRecord: {
+				url: '/v2/objects/companies/records/rec-1',
+				method: 'GET',
+			},
+			getRecordAttributeValues: {
+				url: '/v2/record_attribute_values/rav-1',
+				method: 'GET',
+			},
+			getV2ObjectsObjectRecordsRecordId: {
+				url: '/v2/objects/companies/records/rec-1',
+				method: 'GET',
+			},
+			getTask: {
+				url: '/v2/tasks/task-1',
+				method: 'GET',
+			},
+			getV2WorkspaceMembers: {
+				url: '/v2/workspace_members',
+				method: 'GET',
+			},
+			getWebhook: {
+				url: '/v2/webhooks/wh-1',
+				method: 'GET',
+			},
+			getWorkspaceMember: {
+				url: '/v2/workspace_members/mem-1',
+				method: 'GET',
+			},
+			getWorkspaceRecord: {
+				url: '/v2/objects/workspaces/records/rec-1',
+				method: 'GET',
+			},
+			listAttributeOptions: {
+				url: '/v2/objects/companies/attributes/email_addresses/options',
+				method: 'GET',
+			},
+			listAttributeStatuses: {
+				url: '/v2/objects/companies/attributes/email_addresses/statuses',
+				method: 'GET',
+			},
+			listAttributes: {
+				url: '/v2/objects/companies/attributes',
+				method: 'GET',
+			},
+			listCallRecordings: {
+				url: '/v2/meetings/mtg-1/call_recordings',
+				method: 'GET',
+			},
+			listCompanies: {
+				url: '/v2/objects/companies/records/query',
+				method: 'POST',
+			},
+			listCompanyAttributeValues: {
+				url: '/v2/objects/companies/records/rec-1/attributes/email_addresses/values',
+				method: 'GET',
+			},
+			listCompanyRecordEntries: {
+				url: '/v2/objects/companies/records/rec-1/entries',
+				method: 'GET',
+			},
+			listDealEntries: {
+				url: '/v2/objects/deals/records/rec-1/entries',
+				method: 'GET',
+			},
+			listDealRecordAttributeValues: {
+				url: '/v2/objects/deals/records/rec-1/attributes/email_addresses/values',
+				method: 'GET',
+			},
+			listDealRecords: {
+				url: '/v2/objects/deals/records/query',
+				method: 'POST',
+			},
+			listEntries: {
+				url: '/v2/lists/list-1/entries/query',
+				method: 'POST',
+			},
+			postV2ListsListEntriesQuery: {
+				url: '/v2/lists/list-1/entries/query',
+				method: 'POST',
+				omitFromBody: ['list', 'list_id'],
+			},
+			listListEntries: {
+				url: '/v2/lists/list-1/entries/query',
+				method: 'POST',
+			},
+			listListEntryAttributeValues: {
+				url: '/v2/lists/list-1/entries/ent-1/attributes/email_addresses/values',
+				method: 'GET',
+			},
+			listLists: {
+				url: '/v2/lists',
+				method: 'GET',
+			},
+			listMeetings: {
+				url: '/v2/meetings',
+				method: 'GET',
+			},
+			listNotes: {
+				url: '/v2/notes',
+				method: 'GET',
+			},
+			listObjects: {
+				url: '/v2/objects',
+				method: 'GET',
+			},
+			listPeopleAttributeValues: {
+				url: '/v2/objects/people/records/rec-1/attributes/email_addresses/values',
+				method: 'GET',
+			},
+			listPeopleRecordEntries: {
+				url: '/v2/objects/people/records/rec-1/entries',
+				method: 'GET',
+			},
+			peopleListPersons: {
+				url: '/v2/objects/people/records/query',
+				method: 'POST',
+			},
+			listRecordAttributeValues: {
+				url: '/v2/objects/companies/records/rec-1/attributes/email_addresses/values',
+				method: 'GET',
+			},
+			getRecordEntries: {
+				url: '/v2/objects/companies/records/rec-1/entries/ent-1',
+				method: 'GET',
+			},
+			listRecordEntries: {
+				url: '/v2/objects/companies/records/rec-1/entries',
+				method: 'GET',
+			},
+			listRecords: {
+				url: '/v2/objects/companies/records/query',
+				method: 'POST',
+				omitFromBody: ['object'],
+			},
+			postV2ObjectsObjectRecordsQuery: {
+				url: '/v2/objects/companies/records/query',
+				method: 'POST',
+			},
+			getV2Tasks: {
+				url: '/v2/tasks',
+				method: 'GET',
+			},
+			listThreads: {
+				url: '/v2/threads',
+				method: 'GET',
+			},
+			listUserRecordEntries: {
+				url: '/v2/objects/users/records/rec-1/entries',
+				method: 'GET',
+			},
+			listUserRecords: {
+				url: '/v2/objects/users/records/query',
+				method: 'POST',
+			},
+			listWebhooks: {
+				url: '/v2/webhooks',
+				method: 'GET',
+			},
+			listWorkspaceMembers: {
+				url: '/v2/workspace_members',
+				method: 'GET',
+			},
+			listWorkspaceRecordAttributeValues: {
+				url: '/v2/objects/workspaces/records/rec-1/attributes/email_addresses/values',
+				method: 'GET',
+			},
+			listWorkspaceRecordEntries: {
+				url: '/v2/objects/workspaces/records/rec-1/entries',
+				method: 'GET',
+			},
+			listWorkspaceRecords: {
+				url: '/v2/objects/workspaces/records/query',
+				method: 'POST',
+			},
+			patchRecord: {
+				url: '/v2/objects/companies/records/rec-1',
+				method: 'PATCH',
+			},
+			putV2ObjectsObjectRecordsRecordId: {
+				url: '/v2/objects/companies/records/rec-1',
+				method: 'PUT',
+			},
+			queryRecords: {
+				url: '/v2/objects/companies/records/query',
+				method: 'POST',
+			},
+			searchRecords: {
+				url: '/v2/objects/records/search',
+				method: 'POST',
+			},
+			postV2ObjectsRecordsSearch: {
+				url: '/v2/objects/records/search',
+				method: 'POST',
+			},
+			updateAttribute: {
+				url: '/v2/lists/list-1/attributes/email_addresses',
+				method: 'PATCH',
+				omitFromBody: [
+					'object',
+					'list',
+					'list_id',
+					'attribute',
+					'attribute_id',
+				],
+			},
+			updateCompany: {
+				url: '/v2/objects/companies/records/rec-1',
+				method: 'PATCH',
+			},
+			updateDealRecord: {
+				url: '/v2/objects/deals/records/rec-1',
+				method: 'PATCH',
+			},
+			updateEntry: {
+				url: '/v2/lists/list-1/entries/ent-1',
+				method: 'PATCH',
+			},
+			updateList: {
+				url: '/v2/lists/list-1',
+				method: 'PATCH',
+			},
+			patchV2ListsListEntriesEntryId: {
+				url: '/v2/lists/list-1/entries/ent-1',
+				method: 'PATCH',
+			},
+			putV2ListsListEntriesEntryId: {
+				url: '/v2/lists/list-1/entries/ent-1',
+				method: 'PUT',
+			},
+			updateObject: {
+				url: '/v2/objects/obj-1',
+				method: 'PATCH',
+			},
+			updatePerson: {
+				url: '/v2/objects/people/records/rec-1',
+				method: 'PATCH',
+			},
+			updateRecord: {
+				url: '/v2/objects/companies/records/rec-1',
+				method: 'PATCH',
+			},
+			updateSelectOption: {
+				url: '/v2/lists/list-1/attributes/email_addresses/options/opt-1',
+				method: 'PATCH',
+				omitFromBody: [
+					'object',
+					'list',
+					'list_id',
+					'attribute',
+					'attribute_id',
+					'option',
+					'select_option_id',
+				],
+			},
+			updateStatus: {
+				url: '/v2/objects/companies/attributes/email_addresses/statuses/st-1',
+				method: 'PATCH',
+			},
+			updateTask: {
+				url: '/v2/tasks/task-1',
+				method: 'PATCH',
+			},
+			updateUserRecord: {
+				url: '/v2/objects/users/records/rec-1',
+				method: 'PATCH',
+			},
+			updateWebhook: {
+				url: '/v2/webhooks/wh-1',
+				method: 'PATCH',
+			},
+			updateWorkspaceRecord: {
+				url: '/v2/objects/workspaces/records/rec-1',
+				method: 'PATCH',
+			},
+		};
+
+		const exportedFnNames = Object.entries(Generated)
+			.filter(([, value]) => typeof value === 'function')
+			.map(([name]) => name);
+
+		it('fails if generated exports drift from expectedRoutes', () => {
+			expect([...exportedFnNames].sort()).toEqual(
+				Object.keys(expectedRoutes).sort(),
+			);
+		});
+
+		for (const [name, expected] of Object.entries(expectedRoutes)) {
+			it(`pins ${name} to ${expected.method} ${expected.url}`, async () => {
+				const fn = Generated[name as keyof typeof Generated];
+				expect(typeof fn).toBe('function');
+				const handler = fn as (ctx: never, input: never) => Promise<MockCall>;
+				const res = await handler(localMockCtx as never, pathFixture as never);
+				expect(res.url).toBe(expected.url);
+				expect(res.options.method).toBe(expected.method);
+				if (expected.omitFromBody) {
+					for (const key of expected.omitFromBody) {
+						expect(res.options.body).not.toHaveProperty(key);
+					}
+				}
+			});
 		}
 	});
 });
