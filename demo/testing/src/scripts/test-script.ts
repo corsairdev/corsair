c9f8e10c58e4b864f4f8415a3dda9cc927e9fd0c
Charan-S-K <www.c.tej2000@gmail.com>
2026-08-21 14:21:16 +0530

    Revert "test: register Abyssale in demo"
    This reverts commit b646a02ef99684cb3438507772b99fa7cec46093.

--- demo/testing/src/scripts/test-script.ts
+++ demo/testing/src/scripts/test-script.ts
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: '../.env' });
 
-import { setupCorsair } from 'corsair';
 import { corsair } from '@/server/corsair';
 
 async function setInstagramCredentials() {
@@ -19,31 +18,11 @@ async function setInstagramCredentials() {
 	}
 }
 
-async function runAbyssaleTest() {
-	console.log('Running Abyssale manual test...');
-	try {
-		await corsair.abyssale.keys.set_api_key('mock-abyssale-key');
-		const res = await corsair.abyssale.api.auth.test({});
-		console.log('Abyssale testAuth response:', res);
-	} catch (error) {
-		console.log(
-			'Abyssale testAuth failed (expected with mock key):',
-			error instanceof Error ? error.message : String(error),
-		);
-	}
-}
-
 const main = async () => {
-	await setupCorsair(corsair);
-	await runAbyssaleTest();
-	try {
-		await corsair.slack.api.messages.post({
-			channel: 'general',
-			text: 'hello',
-		});
-	} catch (error) {
-		// Slack might fail if not configured
-	}
+	const res = await corsair.slack.api.messages.post({
+		channel: 'general',
+		text: 'hello',
+	});
 };
 
 main().catch((err) => {
