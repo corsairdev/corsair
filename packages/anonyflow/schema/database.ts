d07b4d89fdb064adb2d1fdc30dd91cd169604264
ambikeesshh <ambikesh2206@gmail.com>
2026-08-23 09:46:07 +0530

    chore(anonyflow): remove generator schema TODO

--- packages/anonyflow/schema/database.ts
+++ packages/anonyflow/schema/database.ts
@@ -1,7 +1,1 @@
-// TODO: Define your database entities here
-// export const AnonyflowExample = z.object({
-// 	id: z.string(),
-// 	name: z.string(),
-// 	created_at: z.coerce.date().nullable().optional(),
-// });
-// export type AnonyflowExample = z.infer<typeof AnonyflowExample>;
+export {};
