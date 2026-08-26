ecb9d94dfca5245dbed5d9b67d1228adfaa84e44
Shebha-M <shebham.06@gmail.com>
2026-08-26 18:27:38 +0530

    feat: add WakaTime integration.

--- demo/testing/src/server/corsair.ts
+++ demo/testing/src/server/corsair.ts
@@ -13,6 +13,7 @@ import { sharepoint } from '@corsair-dev/sharepoint';
 import { slack } from '@corsair-dev/slack';
 import { twilio } from '@corsair-dev/twilio';
 import { vapi } from '@corsair-dev/vapi';
+import { wakatime } from '@corsair-dev/wakatime';
 import { createCorsair } from 'corsair';
 
 import { sqlite } from '../db';
@@ -64,5 +65,8 @@ export const corsair = createCorsair({
 			webhookSecret: process.env.VAPI_WEBHOOK_SECRET,
 		}),
 		instagram(),
+		wakatime({
+			key: process.env.WAKATIME_API_KEY,
+		}),
 	],
 });
