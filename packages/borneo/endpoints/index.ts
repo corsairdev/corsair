a87e46a662e5ecfb64c18f6f8c2872ce308e4c78
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-08-27 14:41:26 +0530

    fix(borneo): move operation inventory outside endpoints

--- packages/borneo/endpoints/index.ts
+++ packages/borneo/endpoints/index.ts
@@ -1,12 +1,12 @@
-export * as Assets from './assets';
 export type {
 	BorneoOperation,
 	BorneoOperationId,
 	BorneoOperationName,
-} from './operations';
+} from '../operations';
 export {
 	BORNEO_OPERATION_COUNT,
 	BORNEO_OPERATION_IDS,
 	BORNEO_OPERATIONS,
-} from './operations';
+} from '../operations';
+export * as Assets from './assets';
 export * as Support from './support';
