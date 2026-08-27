90c0a4200ce1e030e7d094b8cad5590eb810053a
SUNNY MISHRA <sunnymishra.atf@gmail.com>
2026-08-28 02:23:34 +0530

    test: register Beamer in demo

--- demo/testing/src/server/corsair.ts
+++ demo/testing/src/server/corsair.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
 
 import { agentql } from '@corsair-dev/agentql';
+import { beamer } from '@corsair-dev/beamer';
 import { gmail } from '@corsair-dev/gmail';
 import { googlecalendar } from '@corsair-dev/googlecalendar';
 import { googlesheets } from '@corsair-dev/googlesheets';
@@ -13,6 +14,7 @@ import { sharepoint } from '@corsair-dev/sharepoint';
 import { slack } from '@corsair-dev/slack';
 import { twilio } from '@corsair-dev/twilio';
 import { vapi } from '@corsair-dev/vapi';
+import { instagram } from '@corsair-dev/instagram';
 import { createCorsair } from 'corsair';
 
 import { sqlite } from '../db';
@@ -21,8 +23,6 @@ const hubProjectApiKey =
 	process.env.CORSAIR_DEV_API_KEY ?? process.env.CORSAIR_API_KEY!;
 const hubSigningSecret =
 	process.env.CORSAIR_DEV_SIGNING_SECRET ?? process.env.CORSAIR_SIGNING_SECRET!;
-// const hubApiUrl = process.env.HUB_API_URL;
-// const hubOAuthCallbackUrl = process.env.HUB_OAUTH_CALLBACK_URL;
 
 export const corsair = createCorsair({
 	multiTenancy: false,
@@ -33,13 +33,11 @@ export const corsair = createCorsair({
 		onTimeout: 'deny',
 	},
 	hub: {
-		// apiUrl: hubApiUrl,
-		// oauthCallbackUrl: hubOAuthCallbackUrl,
 		projectApiKey: hubProjectApiKey,
 		signingSecret: hubSigningSecret,
 	},
 	plugins: [
-		// github({ authType: 'managed' }),
+		beamer({ key: process.env.BEAMER_API_KEY }),
 		slack({
 			permissions: {
 				mode: 'cautious',
