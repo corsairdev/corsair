16bb5916f858ee24bafb51dbca7739baa8f77e62
SUNNY MISHRA <sunnymishra.atf@gmail.com>
2026-08-28 02:23:46 +0530

    test: exercise Beamer in demo script

--- demo/testing/src/scripts/test-script.ts
+++ demo/testing/src/scripts/test-script.ts
@@ -4,25 +4,13 @@ dotenv.config({ path: '../.env' });
 
 import { corsair } from '@/server/corsair';
 
-async function setInstagramCredentials() {
-	const { FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, IG_ACCESS_TOKEN } = process.env;
-
-	if (FACEBOOK_APP_ID) {
-		await corsair.keys.instagram.set_client_id(FACEBOOK_APP_ID);
-	}
-	if (FACEBOOK_APP_SECRET) {
-		await corsair.keys.instagram.set_client_secret(FACEBOOK_APP_SECRET);
-	}
-	if (IG_ACCESS_TOKEN) {
-		await corsair.instagram.keys.set_access_token(IG_ACCESS_TOKEN);
-	}
-}
-
 const main = async () => {
-	const res = await corsair.slack.api.messages.post({
-		channel: 'general',
-		text: 'hello',
-	});
+	if (process.env.BEAMER_API_KEY) {
+		const posts = await corsair.beamer.api.posts.get({ limit: 1 });
+		console.log('Beamer posts.get succeeded:', posts);
+	} else {
+		console.log('BEAMER_API_KEY is not set; skipping live Beamer request.');
+	}
 };
 
 main().catch((err) => {
