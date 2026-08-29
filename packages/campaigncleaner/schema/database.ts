92ace73dc109441bd79b400599eda32ba06fd34e
avirals14 <saviher@gmail.com>
2026-08-29 16:56:14 +0530

    fix: remove Campaign Cleaner schema boilerplate

--- packages/campaigncleaner/schema/database.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-// TODO: Define your database entities here
-// export const CampaignCleanerExample = z.object({
-// 	id: z.string(),
-// 	name: z.string(),
-// 	created_at: z.coerce.date().nullable().optional(),
-// });
-// export type CampaignCleanerExample = z.infer<typeof CampaignCleanerExample>;
