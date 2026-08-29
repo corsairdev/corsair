e0f6ccf05db632e75b6bbd63721d39c534b8d8ed
Atharv Mahajan <atharvmahaja@gmail.com>
2026-08-29 07:56:18 +0530

    feat: add DreamStudio integrationDream studio (#1175)
    Co-authored-by: Dhirender Choudhary <Dhirenderchoudhary0001@gmail.com>

--- packages/corsair/core/constants.ts
+++ packages/corsair/core/constants.ts
@@ -113,6 +113,7 @@ export const BaseProviders = [
 	'dockerhub',
 	'dodopayments',
 	'doppler',
+	'dreamstudio',
 	'dropbox',
 	'epicgames',
 	'exa',
@@ -328,6 +329,7 @@ export const ProviderDisplayNames = {
 	dockerhub: 'Docker Hub',
 	dodopayments: 'Dodo Payments',
 	doppler: 'Doppler',
+	dreamstudio: 'DreamStudio',
 	dropbox: 'Dropbox',
 	epicgames: 'Epic Games',
 	exa: 'Exa',
@@ -550,6 +552,7 @@ export type AllProviders =
 	| 'dockerhub'
 	| 'dodopayments'
 	| 'doppler'
+	| 'dreamstudio'
 	| 'dropbox'
 	| 'epicgames'
 	| 'exa'
