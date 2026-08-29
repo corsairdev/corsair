1380a5749a4c0bee7da06df7c27ed3fec8de16d3
Mayank Saini <mayanksaraswal@gmail.com>
2026-08-29 17:52:39 +0530

    chore(mailcheck): remove dead schema/database.ts file with TODO comments

--- packages/mailcheck/schema/database.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-// TODO: Define your database entities here
-// export const MailcheckExample = z.object({
-// 	id: z.string(),
-// 	name: z.string(),
-// 	created_at: z.coerce.date().nullable().optional(),
-// });
-// export type MailcheckExample = z.infer<typeof MailcheckExample>;
