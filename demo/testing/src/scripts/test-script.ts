5b61ec29aadcc095ba60f068f8ac15d9db154651
Dhirender Choudhary <Dhirenderchoudhary0001@gmail.com>
2026-09-06 18:38:35 +0530

    fix(chmeetings): drop demo and gitignore from plugin PR

--- demo/testing/src/scripts/test-script.ts
+++ demo/testing/src/scripts/test-script.ts
@@ -1,9 +1,31 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
+
 import { corsair } from '@/server/corsair';
-import 'dotenv/config';
+
+async function setInstagramCredentials() {
+	const { FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, IG_ACCESS_TOKEN } = process.env;
+
+	if (FACEBOOK_APP_ID) {
+		await corsair.keys.instagram.set_client_id(FACEBOOK_APP_ID);
+	}
+	if (FACEBOOK_APP_SECRET) {
+		await corsair.keys.instagram.set_client_secret(FACEBOOK_APP_SECRET);
+	}
+	if (IG_ACCESS_TOKEN) {
+		await corsair.instagram.keys.set_access_token(IG_ACCESS_TOKEN);
+	}
+}
 
 const main = async () => {
-	const person = await corsair.chmeetings.person.get({ id: '4633264' });
-	console.log(person);
+	const res = await corsair.slack.api.messages.post({
+		channel: 'general',
+		text: 'hello',
+	});
 };
 
-main();
+main().catch((err) => {
+	console.error(err);
+	process.exit(1);
+});
